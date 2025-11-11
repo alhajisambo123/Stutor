3c68bccdd553db92c344d3dcc86124b532856f6d
Your GitHub Name <alhajimalamsambo442@gmail.com>
2025-11-11 22:44:07 +0000

    changes1

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,15 +1,11 @@
 import account from "./account";
-import booking from "./booking";
 import course from "./course";
 import review from "./review";
 import user from "./user";
-import verificationToken from "./verificationToken";
 
 export const schemaTypes = [
   user,
   account,
-  booking,
   course,
   review,
-  verificationToken,
 ];
